7141b892e9df017d2aff6e70c9d58a372686a9aa
beasnascimento <102321210+beasnascimento@users.noreply.github.com>
2022-05-03 15:53:43 -0300

    Add files via upload

---
+++ Exercicio3WhileIdades.java
@@ -0,0 +1,29 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio3WhileIdades {
+
+public static void main(String[] args) {
+		
+		
+		int idade=0,menos21=0, mais50=0;
+		Scanner leia = new Scanner(System.in);
+		
+		
+		while(idade!=-99) {
+			System.out.println("\nEntre com as idades: ");
+			idade = leia.nextInt();
+			
+			if(idade<=21 && idade > 0) {
+				menos21++;
+				
+			}else if(idade>=50) {
+				mais50++;
+			}
+			
+		}
+		System.out.println("\nO total de pessoas com menos de 21 anos é: "+menos21);
+		System.out.println("\nO total de pessoas com mais de 50 anos é: "+mais50);
+	}
+}
